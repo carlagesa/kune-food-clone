7b60020e5b3c6ea478d10f0da86d55ea0b671211
carlagesa <carltonmbaja0@gmail.com>
2022-05-27 14:17:57 +0300

    Added icons to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import './Navbar.css';
 import logo from '../logo.png';
+import UserIcon from '../user-icon.png';
+import ShoppingIcon from '../shopping-bag.png';
+
+
 
 
 
 export default function Navbar() {
-    return(
+    return (
         <div>
             <header className='header'>
                 <ul>
-               {/* <button className='location-info'>Location</button> */}
-                <img src={logo} className="kune-logo" alt="logo" />
-                    <li>Home</li>
-                    <li>Menu</li>
-                    <li>Log In</li>
-                 
+                    {/* <button className='location-info'>Location</button> */}
+                    <li className='items' id='items'>0</li>
 
+                    <img src={logo} className="kune-logo" alt="logo" />
+
+                    <li><img src={ShoppingIcon} className="shopping-icon" alt="Shopping" /></li>
+                    <li className='login-text'>Log In</li>
+                    <li><img src={UserIcon} className="user-icon" alt="User" />
+                    </li>
                 </ul>
             </header>
         </div>
